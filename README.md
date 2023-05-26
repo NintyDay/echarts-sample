3f8cde8af7fd65ca427d13c216e4eb0589282e8e
NintyDay <index_0603@outlook.com>
2023-05-26 11:39:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# echarts-sample
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-x2jmxf)
---
+++ src/reuseLogic/echarts.js
@@ -0,0 +1,27 @@
+import { onMounted } from 'vue' 
+import * as Echarts from 'echarts';
+
+export function ech(domContainer) {
+  let mychart;
+  onMounted(() => {
+    mychart = Echarts.init(domContainer.value);
+
+    mychart.setOption({
+      title: {
+        text: 'ECharts 入门示例',
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    });
+  });
+}
